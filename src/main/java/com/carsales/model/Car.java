6c9fe81accb65ae546c7795f7334d2d597923029
Iron-voldy <hasindutw,@gmail.com>
2025-05-16 21:41:23 +0530

    modiify the Car Model

--- src/main/java/com/carsales/model/Car.java
+++ src/main/java/com/carsales/model/Car.java
@@ -25,42 +25,54 @@ public class Car {
         return id;
     }
     public void setId(int id) {
+
         this.id = id;
     }
     public String getMake() {
+
         return make;
     }
     public void setMake(String make) {
+
         this.make = make;
     }
     public String getModel() {
         return model;
     }
     public void setModel(String model) {
+
         this.model = model;
     }
     public int getYear() {
+
         return year;
     }
     public void setYear(int year) {
+
         this.year = year;
     }
     public double getPrice() {
+
         return price;
     }
     public void setPrice(double price) {
+
         this.price = price;
     }
     public String getDescription() {
+
         return description;
     }
     public void setDescription(String description) {
+
         this.description = description;
     }
     public String[] getImages() {
+
         return images;
     }
     public void setImages(String[] images) {
+
         this.images = images;
     }
 }
